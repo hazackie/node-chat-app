ad37359c07f0814272b23892a1eba0f3f72e580a
Ha Hoang <hah@Has-MacBook-Air.local>
2017-09-14 10:49:31 +0700

    add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Hoang Ha',
+    text: 'Yup, I love you too'
+  });
+});
+
+socket.on('newMessage', (message) => {
+  console.log('newMessage', message);
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from server');
+});
--- server/server.js
+++ server/server.js
@@ -16,6 +16,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Mike',
+    text: 'I just came to my house.'
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
