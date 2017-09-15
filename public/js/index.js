9d78ab6bf07cc3a3b24d2bb4b0600d22fc7b5584
Ha Hoang <hah@Has-MacBook-Air.local>
2017-09-15 10:48:25 +0700

    add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,8 +11,30 @@ socket.on('connect', () => {
 
 socket.on('newMessage', (message) => {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
+});
+
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi'
+// }, (data) => {
+//   console.log('Got it', data);
+// });
+
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name="message"]').val()
+  }, function () {
+
+  });
 });
